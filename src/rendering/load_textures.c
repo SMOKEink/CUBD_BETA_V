bcea0069cbd6e0f6044c2e805d068374fb058ddc
Youssef Hajbi <yhajbi@e2r5p12.1337.ma>
2025-12-17 16:17:38 +0100

    fixed a leak when textures arent loaded properly

--- src/rendering/load_textures.c
+++ src/rendering/load_textures.c
@@ -6,12 +6,14 @@ int load_tex_any(t_game *gm, t_tex *tex, const char *path)
 	if (!tex->img)
 	{
 		fprintf(stderr, "Texture load failed: %s\n", path);
+		destroy_game(gm, "DESTROY_TEST");					// added by yhajbi to handle a leak
 		return 1;
 	}
 	tex->data = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len, &tex->endian);
 	if (!tex->data)
 	{
 		fprintf(stderr, "Data addr failed: %s\n", path);
+		destroy_game(gm, "DESTROY_TEST");					// added by yhajbi to handle a leak
 		return 1;
 	}
 	return 0;
@@ -24,12 +26,14 @@ int load_tex(t_game *gm, int idx, const char *path)
 	if (!t->img)
 	{
 		fprintf(stderr, "Texture load failed: %s\n", path);
+		destroy_game(gm, "DESTROY_TEST");					// added by yhajbi to handle a leak
 		return 1;
 	}
 	t->data = mlx_get_data_addr(t->img, &t->bpp, &t->line_len, &t->endian);
 	if (!t->data)
 	{
 		fprintf(stderr, "Data addr failed: %s\n", path);
+		destroy_game(gm, "DESTROY_TEST");					// added by yhajbi to handle a leak
 		return 1;
 	}
 	return 0;
