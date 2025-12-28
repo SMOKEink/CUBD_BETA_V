0bd710c9f224a4080479a9cf41304f3dbdee6815
Makevali <aachata@student.1337.ma>
2025-12-28 10:43:48 +0100

    load textures

--- mandatory/src/rendering/load_textures.c
+++ mandatory/src/rendering/load_textures.c
@@ -6,31 +6,11 @@
 /*   By: makevali <makevali@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/24 21:01:42 by aachata           #+#    #+#             */
-/*   Updated: 2025/12/28 10:28:25 by makevali         ###   ########.fr       */
+/*   Updated: 2025/12/28 10:39:07 by makevali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3d.h"
-
-int	load_tex_any(t_game *gm, t_tex *tex, const char *path)
-{
-	tex->img = mlx_xpm_file_to_image(gm->mlx, (char *)path, &tex->w, &tex->h);
-	if (!tex->img)
-	{
-		printf("Texture load failed: %s\n", path);
-		destroy_game(gm, "Quitting...");
-		return (1);
-	}
-	tex->data = mlx_get_data_addr(tex->img, &tex->bpp, &tex->line_len,
-			&tex->endian);
-	if (!tex->data)
-	{
-		printf("Data addr failed: %s\n", path);
-		destroy_game(gm, "Quitting...");
-		return (1);
-	}
-	return (0);
-}
 
 int	load_tex(t_game *gm, int idx, const char *path)
 {
